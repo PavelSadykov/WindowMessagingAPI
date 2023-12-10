6df6ecee3002f85844ef1bd3612a79485d9ebb54
PavelSadykov <2439747@gmail.com>
2023-12-10 15:20:13 +0500

    Добавьте файлы проекта.

---
+++ WindowMessagingAPI/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace WindowMessagingAPI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNewTitle = new System.Windows.Forms.TextBox();
+            this.btnChangeTitle = new System.Windows.Forms.Button();
+            this.btnCloseWindow = new System.Windows.Forms.Button();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNewTitle
+            // 
+            this.txtNewTitle.Location = new System.Drawing.Point(288, 107);
+            this.txtNewTitle.Name = "txtNewTitle";
+            this.txtNewTitle.Size = new System.Drawing.Size(100, 20);
+            this.txtNewTitle.TabIndex = 0;
+            // 
+            // btnChangeTitle
+            // 
+            this.btnChangeTitle.Location = new System.Drawing.Point(394, 104);
+            this.btnChangeTitle.Name = "btnChangeTitle";
+            this.btnChangeTitle.Size = new System.Drawing.Size(110, 23);
+            this.btnChangeTitle.TabIndex = 1;
+            this.btnChangeTitle.Text = "Изменить ";
+            this.btnChangeTitle.UseVisualStyleBackColor = true;
+            this.btnChangeTitle.Click += new System.EventHandler(this.btnChangeTitle_Click_1);
+            // 
+            // btnCloseWindow
+            // 
+            this.btnCloseWindow.Location = new System.Drawing.Point(394, 170);
+            this.btnCloseWindow.Name = "btnCloseWindow";
+            this.btnCloseWindow.Size = new System.Drawing.Size(110, 23);
+            this.btnCloseWindow.TabIndex = 2;
+            this.btnCloseWindow.Text = "Закрыть";
+            this.btnCloseWindow.UseVisualStyleBackColor = true;
+            this.btnCloseWindow.Click += new System.EventHandler(this.btnCloseWindow_Click_1);
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(288, 45);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(100, 20);
+            this.textBoxInput.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(394, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Поиск окна";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(145, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Введите название окна";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(109, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Укажите новое название окна";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.btnCloseWindow);
+            this.Controls.Add(this.btnChangeTitle);
+            this.Controls.Add(this.txtNewTitle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNewTitle;
+        private System.Windows.Forms.Button btnChangeTitle;
+        private System.Windows.Forms.Button btnCloseWindow;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowMessagingAPI/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace WindowMessagingAPI
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        const uint WM_CLOSE = 0x0010;
+        const uint WM_SETTEXT = 0x000C;
+        string nameWindow = "";
+        string newTitle = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        //обработка события для  поиска окна
+        private void button1_Click(object sender, EventArgs e)
+        {
+            nameWindow = textBoxInput.Text;//Вводим название
+            IntPtr windowHandle = FindWindow(null, nameWindow);
+
+            if (windowHandle != IntPtr.Zero)// проверяем наличие
+            {
+
+                MessageBox.Show("Окно : " + nameWindow, "Успешно найдено");
+            }
+            else
+            {
+                MessageBox.Show("Окно не найдено", "Ошибка");
+            }
+
+
+        }
+
+        //обработка события для  изменения названия  окна
+        private void btnChangeTitle_Click_1(object sender, EventArgs e)
+        {
+            newTitle = txtNewTitle.Text;// вводим новое название
+          
+
+            IntPtr windowHandle = FindWindow(null, nameWindow);
+
+            if (windowHandle != IntPtr.Zero)// проверяем наличие окна с существующим названием
+            {
+                SendMessage(windowHandle, WM_SETTEXT, IntPtr.Zero, Marshal.StringToBSTR(newTitle));// меняем название
+                MessageBox.Show("Заголовок окна изменен на: " + newTitle, "Успешно");
+            }
+            else
+            {
+                MessageBox.Show("Окно не найдено", "Ошибка");
+            }
+        }
+        //обработка события закрытия окна
+        private void btnCloseWindow_Click_1(object sender, EventArgs e)
+        {
+            IntPtr windowHandle = FindWindow(null, newTitle);
+
+            if (windowHandle != IntPtr.Zero)// если окно с новым названием существует
+            {
+                SendMessage(windowHandle, WM_CLOSE, IntPtr.Zero, IntPtr.Zero);// закрываем его
+                MessageBox.Show("Окно закрыто", "Успешно");
+            }
+            else
+            {
+                MessageBox.Show("Окно не найдено", "Ошибка");
+            }
+        }
+       
+    }
+}
